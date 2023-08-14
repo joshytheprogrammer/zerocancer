d03fc2c5cd62e638255259abdb2611685110723a
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-08-14 12:35:07 +0100

    made changes: 'implmented customer changes'

--- script.js
+++ script.js
@@ -41,3 +41,15 @@ addToHomeButton.addEventListener('click', () => {
     });
   }
 });
+
+// ReadMore feature
+function showRemaining() {
+  const paragraphs = document.querySelectorAll('.toShowText');
+  for (let i = 3; i < paragraphs.length; i++) {
+    paragraphs[i].style.display = 'block';
+    paragraphs[i].classList.add('animate__animated', 'animate__fadeIn');
+  }
+
+  // Hide the "Read More" link after showing remaining paragraphs
+  document.getElementById('readMoreLink').style.display = 'none';
+}
